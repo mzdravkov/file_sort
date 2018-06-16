1ce9e54a84af91d92ea5fecd243fa8868f64e351
Mihail Zdravkov <mihail0zdravkov@gmail.com>
2018-06-16 13:03:48 +0300

    Try to use Trie (requires awfuly lot memory); Implement one-pass bucket sort with insertion sort for nextSort

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
 	"io"
 	"os"
@@ -11,9 +12,10 @@ var sorterPool = make(chan chan []byte, 8)
 var writerInput = make(chan []byte)
 var readerFinished = make(chan int)
 var writerFinished = make(chan struct{})
+var newLine = []byte("\n")[0]
 
 func init() {
-	for i := 0; i < 8; i++ {
+	for i := 0; i < 1; i++ {
 		sorterPool <- createSorter()
 	}
 }
@@ -33,9 +35,122 @@ func createSorter() chan []byte {
 	return ch
 }
 
+func insertionSort(lines [][]byte) {
+	for i := 1; i < len(lines); i++ {
+		for j := i; j > 0 && bytes.Compare(lines[j-1], lines[j]) > 1; j-- {
+			lines[j-1], lines[j] = lines[j], lines[j-1]
+		}
+	}
+}
+
+func nextSort(lines [][]byte, signalFinishChan chan struct{}) {
+	insertionSort(lines)
+
+	signalFinishChan <- struct{}{}
+}
+
+func bucketSort(buff []byte) [][]byte {
+	buckets := make([][][]byte, 256)
+
+	reader := bufio.NewReader(bytes.NewBuffer(buff))
+
+	for {
+		line, err := reader.ReadBytes(newLine)
+		// TODO: is it sure that I should break here or should I loop once more?
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			panic(err)
+		}
+
+		// TODO: think about zero-length lines
+
+		bucketIndex := int(line[0])
+		buckets[bucketIndex] = append(buckets[bucketIndex], line)
+	}
+
+	signalFinishChan := make(chan struct{}, 256)
+	for _, bucket := range buckets {
+		go nextSort(bucket, signalFinishChan)
+	}
+	for i := 0; i < 256; i++ {
+		<-signalFinishChan
+	}
+
+	result := buckets[0]
+	for _, bucket := range buckets[1:] {
+		result = append(result, bucket...)
+	}
+
+	fmt.Println(result[:3])
+
+	return result
+}
+
 func sort(buff []byte) {
 	fmt.Printf("Starting to sort %d bytes of data.\n", len(buff))
+	bucketSort(buff)
+	// trie := CreateTrie(buff)
+	// fmt.Println(trie)
 }
+
+// type TrieNode struct {
+// 	// val      []byte
+// 	val      byte
+// 	isLeaf   bool
+// 	children map[byte]*TrieNode
+// }
+
+// type Trie struct {
+// 	root *TrieNode
+// }
+
+// func CreateTrie(buff []byte) *Trie {
+// 	fmt.Println("CREATE TRIE")
+// 	t := new(Trie)
+// 	t.root = new(TrieNode)
+// 	t.root.isLeaf = false
+// 	t.root.children = make(map[byte]*TrieNode)
+
+// 	reader := bufio.NewReader(bytes.NewBuffer(buff))
+
+// 	newLine := []byte("\n")[0]
+// 	for {
+// 		line, err := reader.ReadBytes(newLine)
+// 		// TODO: is it sure that I should break here or should I loop once more?
+// 		if err != nil {
+// 			if err == io.EOF {
+// 				fmt.Println("BREAAK")
+// 				break
+// 			}
+// 			panic(err)
+// 		}
+
+// 		word := line[:len(line)-1]
+
+// 		currentNode := t.root
+// 		for i := 0; i < len(word)-1; i++ {
+
+// 			if _, ok := currentNode.children[word[i]]; !ok {
+// 				newChild := new(TrieNode)
+// 				// TODO: do I need the val of non-leaf trie nodes?
+// 				// newChild.val = []byte{word[i]}
+// 				newChild.val = word[i]
+// 				newChild.isLeaf = false
+// 				newChild.children = make(map[byte]*TrieNode)
+// 				currentNode.children[word[i]] = newChild
+// 			}
+// 			currentNode = currentNode.children[word[i]]
+// 		}
+// 		leafNode := new(TrieNode)
+// 		// leafNode.val = word
+// 		leafNode.val = word[len(word)-1]
+// 		leafNode.isLeaf = true
+// 	}
+
+// 	return t
+// }
 
 func partitioningReader(filename string) {
 	file, err := os.Open(filename)
@@ -51,6 +166,7 @@ func partitioningReader(filename string) {
 	partitionsRead := 0
 
 	for {
+		// TODO: think about lines longer than bufferSize
 		bufferSize := 10 * 1024 * 1024
 		buff := make([]byte, bufferSize)
 
@@ -63,7 +179,7 @@ func partitioningReader(filename string) {
 			break
 		}
 
-		newLine := []byte("\n")[0]
+		// newLine := []byte("\n")[0]
 		cutAt := len(buff) - 1
 		for ; cutAt >= 0; cutAt-- {
 			if buff[cutAt] == newLine {
